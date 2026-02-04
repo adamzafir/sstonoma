7ce843a645a99d2b4d7c5996d2de0707c9c0fd71
adamzafir <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2026-02-04 21:11:57 +0800

    feat: acceleration

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub()
+grabby_thingie = Motor(Port.F)
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.B,positive_direction=Direction.COUNTERCLOCKWISE)
 
-left_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
-right_motor = Motor(Port.B)
 
 drive_base = DriveBase(
     left_motor,
@@ -19,9 +20,11 @@ drive_base = DriveBase(
 left_sensor = ColorSensor(Port.C)
 right_sensor = ColorSensor(Port.D)
 
-def line_track(speed: double, kp: double, kd: double, distance: double):
+def line_track(maxspeed: double, kp: double, kd: double, distance: double):
     perror = 0
     left_motor.reset_angle()
+    initialspeed = 12.3
+    speed = initialspeed
     while left_motor.angle() < distance:
         error = left_sensor.reflection() - right_sensor.reflection()
         p = error * kp
@@ -29,6 +32,12 @@ def line_track(speed: double, kp: double, kd: double, distance: double):
         left_motor.dc(speed - (p+d))
         right_motor.dc(speed + (p+d))
         perror = error
+        if speed < maxspeed:
+            speed += 0.23
+            print(speed)
     drive_base.stop()
 
-line_track(speed=80,kp=0.5,kd=3,distance=1000)
+line_track(maxspeed=100,kp=0.255,kd=6.7,distance=10000)
+
+#testing for junction
+print(f"left sensor: {left_sensor.reflection}, right sensor: {right_sensor.reflection}")
